60e4aaa0ae1c196b5e87dfce5383176dd601e7da
iyustlop <iyustlop@gmail.com>
2018-04-06 15:31:41 +0200

    Readme included

--- dsd-controller/src/main/java/com/freelance/spring/main/ApplicationMain.java
+++ dsd-controller/src/main/java/com/freelance/spring/main/ApplicationMain.java
@@ -4,10 +4,12 @@ import com.freelance.spring.domain.Car;
 import com.freelance.spring.repository.CarRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.stream.Stream;
@@ -15,6 +17,7 @@ import java.util.stream.Stream;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.freelance.spring.repository"})
 @EntityScan(basePackages = {"com.freelance.spring.domain"})
+@EnableAutoConfiguration
 public class ApplicationMain {
     public static void main(String[] args){
         SpringApplication.run(ApplicationMain.class);
