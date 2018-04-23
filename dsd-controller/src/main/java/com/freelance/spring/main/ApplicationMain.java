da0e03772b3304874a651fe0b54e3c54eed50243
iyustlop <iyustlop@gmail.com>
2018-04-23 23:53:41 +0200

    Controller Creation. Pending refinement.

--- dsd-controller/src/main/java/com/freelance/spring/main/ApplicationMain.java
+++ dsd-controller/src/main/java/com/freelance/spring/main/ApplicationMain.java
@@ -8,31 +8,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.stream.Stream;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.freelance.spring.repository"})
 @EntityScan(basePackages = {"com.freelance.spring.domain"})
 @EnableAutoConfiguration
 public class ApplicationMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class);
     }
 
-    @Bean
-    ApplicationRunner init(CarRepository repository) {
-        return args -> {
-            Stream.of("Ferrari", "288","Jaguar", "E-Type","Porsche", "964" ,"Lamborghini", "Miura",
-                    "Bugatti", "Veyron").forEach(name -> {
-                Car car = new Car();
-                car.setManufacturer(name);
-                repository.save(car);
-            });
-            repository.findAll().forEach(System.out::println);
-        };
-    }
 }
