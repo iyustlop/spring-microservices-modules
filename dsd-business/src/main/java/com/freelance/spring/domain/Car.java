cc8877a2df94ed54dee301068f8b61e10b34c50f
iyustlop <iyustlop@gmail.com>
2018-04-23 23:51:08 +0200

    Minor refinement

--- dsd-business/src/main/java/com/freelance/spring/domain/Car.java
+++ dsd-business/src/main/java/com/freelance/spring/domain/Car.java
@@ -3,10 +3,8 @@ package com.freelance.spring.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
