18ec2157063dbddf5f77257146bf3060487e0777
ignacio Yuste López <iyustlop@gmail.com>
2025-10-08 21:17:54 +0200

    update MockBean to MockitoBean

--- dsd-controller/src/test/java/com/freelance/spring/controller/ControllerTest.java
+++ dsd-controller/src/test/java/com/freelance/spring/controller/ControllerTest.java
@@ -6,9 +6,9 @@ import com.freelance.spring.repository.CarRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -28,10 +28,10 @@ public class ControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private CarRepository carRepository;
 
-    @MockBean
+    @MockitoBean
     private MongoTemplate mongoTemplate1;
 
     @Test
--- dsd-controller/src/test/java/com/freelance/spring/controller/LlamadaControllerTest.java
+++ dsd-controller/src/test/java/com/freelance/spring/controller/LlamadaControllerTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -28,16 +28,16 @@ public class LlamadaControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private LlamadaService llamadaService;
 
-    @MockBean
+    @MockitoBean
     private CarService carService;
 
-    @MockBean
+    @MockitoBean
     private CarRepository carRepository;
 
-    @MockBean
+    @MockitoBean
     private MongoTemplate mongoTemplate;
 
     @Autowired
